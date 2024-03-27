116f2ac44072f2a1ef842b2cac871dd362b9bb93
ravvi1104 <rry4298@gmail.com>
2024-03-27 19:16:34 +0530

    add logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
-
 const Navbar = () => {
   return (
     <>
       <div className="navbar">
-        <Link to="/" className="nav-img"><img src="./logo.png"></img></Link>
+        <Link to="/" className="nav-img"><img src="./logo.png" alt="logo"></img></Link>
         <Link to='/Habit-Detail/today' className="nav-link">Details</Link>
         <Link to='/' className="nav-link">WeekView</Link>
       </div>
